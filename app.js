4f9165c90e18920a37108ed5e12ca7eb74f43947
Zventus <48690157+Zventus@users.noreply.github.com>
2019-04-25 11:22:25 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ firebase.initializeApp(config);
    var active2 = false;
    var active3 = false;
    var active4 = false;
-   var active5 = false;        
+   var active5 = false;
+   var active6 = false;        
 //*******************************************************************************************/
    function register () {
     var email    = document.getElementById("email").value;
     var password = document.getElementById("password").value;
       firebase.auth().createUserWithEmailAndPassword(email, password)
-          .then(function(){
-            document.getElementById("exampleModal").style.display = "none";      
+          .then(function(){     
             verify();    
           })
           .catch(function(error) {
@@ -39,36 +39,12 @@ firebase.initializeApp(config);
     var user = firebase.auth().currentUser;
         user.sendEmailVerification().then(function() {
 // Email sent.
-      // setTimeout(observer, 1000);
       console.log('sending email...');
     }).catch(function(error) {
 // An error happened.
       console.log(error);
   });   
 }
-  function save () {     
-    var user = firebase.auth().currentUser;
-    var email = user.email; 
-    var test = document.getElementById("my_timer");
-    var act = test.textContent;
-    var test2 = document.getElementById("my_timer2");
-    var act2 = test2.textContent;
-    var test3 = document.getElementById("my_timer3");
-    var act3 = test3.textContent;
-    var test4 = document.getElementById("my_timer4");
-    var act4 = test4.textContent;
-    var test5 = document.getElementById("my_timer5");
-    var act5 = test5.textContent;
-    var DocRef2 = db.collection(resultDate).doc(email);
-      DocRef2.set({  
-          Status: { total: act ,
-          lunch: act2 ,
-          break: act3 , 
-          train: act4 ,
-          project: act5 ,  
-          login: resultTime }
-   }, {merge: true});
-  }
 //*******************************************************************************/
   function out () {
       firebase.auth().signOut();
@@ -91,7 +67,7 @@ firebase.initializeApp(config);
         }, {merge: true});
     }, 500)   
 }
-//**********************************************************************/
+//--------------------------------------------------------------------------/
 function logout() {
     var date = new Date();
     var options  = { month: 'short', day: '2-digit', weekday: 'short'};
@@ -136,15 +112,15 @@ function logout() {
       var resultTime = new Intl.DateTimeFormat('en-GB', options2).format(date);
         var email = user.email;
         var emailVerified = user.emailVerified;
-          console.log('user:',email);
-          console.log(emailVerified); 
-          console.log('***************************');
+            console.log('user:', email);
+            document.getElementById("color").style.display = "none";
           if (emailVerified) {
             document.getElementById("user_div").style.display = "block";
             document.getElementById("test").style.display = "none";
             document.getElementById("test10").innerHTML = "Welcome" + " " + name + "!";
             changeState();
-              console.log('hey there');
+            changeState6();
+              // console.log('hey there');
               var DocRef = db.collection(resultDate).doc(email);
       DocRef.set({  
           User: { login: resultTime,
@@ -164,8 +140,6 @@ function logout() {
         }
 //*************************************************************************************************/
 
-
-//******************************************************************************************/
 // Three function for three timers. Each status has its own count timer
 function start_timer() {
   // This function will go if active is true
@@ -309,6 +283,34 @@ function start_timer5() {
     }
 }
 
+function start_timer6() {
+  // This function will go if active is true
+  if (active6) {
+    var timer = document.getElementById("my_timer6").innerHTML;
+    var arr = timer.split(":");
+    var hour = arr[0]; // getting hour
+    var min = arr[1];  // minutes
+    var sec = arr[2]; // seconds
+
+  if (sec == 59) {
+    if (min == 59) {
+      hour++;
+      min = 0;
+     if (hour < 10) hour = "0" + hour;
+    } else { 
+      min++;
+        }
+        if (min < 10) min = "0" + min;
+        sec = 0;
+       } else {
+           sec ++;
+          if (sec < 10) sec = "0" + sec;
+     }
+     // update our html
+     document.getElementById("my_timer6").innerHTML = hour + ":" + min + ":"+ sec;
+     setTimeout(start_timer6, 1000); // keep repeating with the speed of 1 sec
+    }
+}
 //*************************************************Change state functions************************** */
 // now we need function to change states - start or pause timer by clicking
 function changeState() {
@@ -363,26 +365,38 @@ function changeState5() {
   console.log("timmer is on pause");
  }
 }
+
+function changeState6() {
+  if (active6 == false) {
+     active6 = true;
+     start_timer6();
+     console.log("Timer has been started");
+ } else {
+  console.log("timmer is on pause");
+ }
+}
 //*********************************Dropdown control *****************************************************/
-function captureAct() {
+function captureAvai() {
+    changeState6();
   active2 = false; 
   active3 = false;
   active4 = false;
   active5 = false;  
 }
+
 function captureLunch () { 
     changeState2(); 
-    console.log("After changeState2 function:", active2);
-  // active = false;
   active3 = false;
   active4 = false;
   active5 = false;
+  active6 = false;
 }
 function captureBreak () { 
    changeState3();
   active2 = false;
   active4 = false;
   active5 = false;
+  active6 = false;
 }
 
 function captureTrain () { 
@@ -390,6 +404,7 @@ function captureTrain () {
   active2 = false;
   active3 = false;
   active5 = false;
+  active6 = false;
     
 }
 function test3() {
@@ -397,6 +412,7 @@ function test3() {
   active2 = false;
   active3 = false;
   active4 = false;
+  active6 = false;
 }
 
  //*************************************************************************************/
@@ -413,32 +429,8 @@ active4 = false;
 active5 = false;
 console.log('Reset done');
 }
-//**********************************************************************/
-// firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     // User is signed in, then Welcome! and time starts running
-//     document.getElementById("user_div").style.display = "block";
-//     document.getElementById("test").style.display = "none";
-//     document.getElementById("test10").innerHTML = "Welcome" + " " + name + "!";
-//     // document.getElementById("my_timer5").innerHTML =  '00:00:00' ;
-    
-//        changeState();
-//     } else {
-//       // No user is signed in.
-//     document.getElementById("user_div").style.display = "none";
-//     document.getElementById("test").style.display = "block";
-//   }
-// });
-//----------------------------------------------------------------------------------------------
 
-//**************************************************************Brand New Tests*****************************/
-  /* Every time the user refresh the browser tha data must be save it to firestore,
-  so the timers must add up. */
 
-  
-  
- 
-//**********************************************************************/
 
 
 
